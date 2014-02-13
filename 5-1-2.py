b79c09a617154ee9f18a16a08c41411e57eeb0e7
ceciet <dq0572255@126.com>
2014-02-13 17:30:25 +0800

    updated

--- 5-1-2.py
+++ 5-1-2.py
@@ -2,6 +2,37 @@ __author__ = 'qdiao'
 import random
 import math
 
+def biasedRandom():
+
+    i = random.randint(0, 3)
+    if i >= 1:
+        i = 1
+    return i
+
+
+def equalRandom():
+
+    while True:
+        i = biasedRandom()
+        j = biasedRandom()
+        if (i+j)!=1:
+            continue
+        if i == 0 and j == 1:
+            return 0
+        if i == 1 and j == 0:
+            return 1
+
+def test():
+
+    m = 0
+    n = 0
+    for i in range(0, 501):
+        i = equalRandom()
+        if i == 1:
+            m += 1
+        else:
+            n += 1
+    return m, n
 
 def randomAB(a, b):
 
@@ -17,3 +48,5 @@ def randomAB(a, b):
 if __name__ == "__main__":
 
     print randomAB(10, 20)
+    #equalRandom()
+    print test()
